3d1561ceb9ce4101674324adf9c1f1e8ab58b777
jmoriatie <jeongmin9102@naver.com>
2023-01-01 15:55:13 +0900

    # domain 리펙토링

--- dbless/src/main/kotlin/com/comento/dbless/domain/Person.kt
+++ dbless/src/main/kotlin/com/comento/dbless/domain/Person.kt
@@ -3,9 +3,16 @@ package com.comento.dbless.domain
 import com.comento.dbless.dto.PersonDto
 
 class Person private constructor(val id : String) {
-    var name : String = ""
-    var age : Int = -1
-    var height : Int = -1
+    val name : String
+        get() = this._name
+    val age : Int
+        get() = this.age
+    val height : Int
+        get() = this._height
+
+    var _name : String = ""
+    var _age : Int = -1
+    var _height : Int = -1
 
     companion object{
         fun from(id : String,
@@ -13,17 +20,17 @@ class Person private constructor(val id : String) {
                  age : Int,
                  height : Int): Person{
             val newPerson = Person(id)
-            newPerson.name = name
-            newPerson.age = age
-            newPerson.height = height
+            newPerson._name = name
+            newPerson._age = age
+            newPerson._height = height
             return newPerson
         }
 
         fun of(personDto : PersonDto) : Person{
             val newPerson = Person(personDto.id)
-            newPerson.name = personDto.name
-            newPerson.age = personDto.age
-            newPerson.height = personDto.height
+            newPerson._name = personDto.name
+            newPerson._age = personDto.age
+            newPerson._height = personDto.height
             return newPerson
         }
     }
