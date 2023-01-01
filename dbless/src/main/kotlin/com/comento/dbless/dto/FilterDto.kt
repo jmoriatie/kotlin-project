499278bd9b05accfd4759295fbde15f9100a38ab
jmoriatie <jeongmin9102@naver.com>
2023-01-01 15:55:41 +0900

    # person dto 리펙토링

--- dbless/src/main/kotlin/com/comento/dbless/dto/FilterDto.kt
+++ dbless/src/main/kotlin/com/comento/dbless/dto/FilterDto.kt
@@ -1,8 +1,8 @@
 package com.comento.dbless.dto
 
-class FilterDto {
-    lateinit var persons : MutableList<PersonDto>
-    var ageCutoff : Int? = null
-    var heightCutoff : Int? = null
+data class FilterDto(
+    var persons : MutableList<PersonDto>,
+    var ageCutoff : Int? = null,
+    var heightCutoff : Int? = null,
     var except : MutableList<String>? = null
-}
+)
--- dbless/src/main/kotlin/com/comento/dbless/dto/SortDto.kt
+++ dbless/src/main/kotlin/com/comento/dbless/dto/SortDto.kt
@@ -1,7 +1,7 @@
 package com.comento.dbless.dto
 
-class SortDto {
-    lateinit var persons : MutableList<PersonDto>
-    lateinit var sortBy : String
-    lateinit var sortOrder : String
-}
+class SortDto(
+    val persons : MutableList<PersonDto>,
+    val sortBy : String,
+    val sortOrder : String
+)
