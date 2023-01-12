bd4479bfbecb806d07976a07d290b0fcd08879d0
jmoriatie <jeongmin9102@naver.com>
2023-01-12 17:27:22 +0900

    # couple matching dto 작성

---
+++ jpa/src/main/kotlin/com/comento/jpa/domain/person/dto/BlindDateDto.kt
@@ -0,0 +1,30 @@
+package com.comento.jpa.domain.person.dto
+
+import com.comento.jpa.domain.person.Person
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+data class BlindDateDto(
+    val name: String,
+    val gender: Gender,
+    val age: Int,
+    val height: Int?,
+    val weight: Int?,
+    val company: String?,
+    val country: String
+){
+
+    companion object{
+        fun from(person: Person): BlindDateDto =
+            BlindDateDto(
+                person.name,
+                person.gender ?: Gender.UNKNOWN,
+                person.age ?: 0,
+                person.height,
+                person.weight,
+                person.company,
+                person.country
+            )
+    }
+}
